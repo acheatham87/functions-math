80f5d1e11905fe59d89ce09b8f245cacfdd33e6b
acheatham87 <acheatham87@gmail.com>
2022-02-17 13:55:35 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const add = (num1, num2) => {
+    let sum = num1 + num2
+    return sum
+}
+const result = add(14, 17) 
+console.log(result) 
+
+const double = (num1) => {
+    let doubled = num1 * 2
+    return doubled
+}
+const resultDoubled = double(result)
+console.log(resultDoubled)
